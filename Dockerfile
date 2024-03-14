aa034b7b8bb2446dd72323d870a201f03ef5285f
Leaf <58620209+Leaf48@users.noreply.github.com>
2024-03-14 12:57:52 -0700

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:lts
+
+WORKDIR /app
+
+COPY . /app/
+
+RUN npm i
+
+CMD [ "npm", "run", "start" ]
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+import { createBot } from "mineflayer";
+dotenv.config();
+
+const init = () => {
+  const bot = createBot({
+    host: process.env.host,
+    port: process.env.port,
+    username: process.env.username,
+    auth: "offline", // Change here to "microsoft" when authentication needed
+  });
+  console.log("Connecting...");
+
+  const changeView = async () => {
+    const yaw = Math.random() * Math.PI - 0.5 * Math.PI,
+      pitch = Math.random() * Math.PI - 0.5 * Math.PI;
+
+    await bot.look(yaw, pitch, false);
+    return;
+  };
+
+  bot.on("login", () => {
+    bot.chat("こんにちは");
+    setInterval(async () => {
+      await changeView();
+    }, 1000 * 3);
+  });
+
+  bot.on("end", (reason) => {
+    console.error(reason);
+    console.log("Reconnecting...");
+    setTimeout(() => {
+      init();
+    }, 1000 * 10);
+  });
+
+  bot.on("error", (err) => {
+    console.log(err);
+    console.log("Reconnecting...");
+    setTimeout(() => {
+      init();
+    }, 1000 * 10);
+  });
+};
+
+init();
