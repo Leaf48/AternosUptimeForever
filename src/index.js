44a72938634f9edbaa638e2434a38f22af3f50eb
leaf48 <watavita2004@gmail.com>
2024-03-14 13:37:25 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,44 +3,48 @@ import { createBot } from "mineflayer";
 dotenv.config();
 
 const init = () => {
-  const bot = createBot({
-    host: process.env.host,
-    port: process.env.port,
-    username: process.env.username,
-    auth: "offline", // Change here to "microsoft" when authentication needed
-  });
-  console.log("Connecting...");
+  try {
+    const bot = createBot({
+      host: process.env.host,
+      port: process.env.port,
+      username: process.env.username,
+      auth: "offline", // Change here to "microsoft" when authentication needed
+    });
+    console.log("Connecting...");
 
-  const changeView = async () => {
-    const yaw = Math.random() * Math.PI - 0.5 * Math.PI,
-      pitch = Math.random() * Math.PI - 0.5 * Math.PI;
+    const changeView = async () => {
+      const yaw = Math.random() * Math.PI - 0.5 * Math.PI,
+        pitch = Math.random() * Math.PI - 0.5 * Math.PI;
 
-    await bot.look(yaw, pitch, false);
-    return;
-  };
+      await bot.look(yaw, pitch, false);
+      return;
+    };
 
-  bot.on("login", () => {
-    bot.chat("こんにちは");
-    setInterval(async () => {
-      await changeView();
-    }, 1000 * 3);
-  });
+    bot.on("login", () => {
+      // bot.chat("こんにちは");
+      setInterval(async () => {
+        await changeView();
+      }, 1000 * 3);
+    });
 
-  bot.on("end", (reason) => {
-    console.error(reason);
-    console.log("Reconnecting...");
-    setTimeout(() => {
-      init();
-    }, 1000 * 10);
-  });
+    bot.on("end", (reason) => {
+      console.error(reason);
+      console.log("Reconnecting...");
+      setTimeout(() => {
+        init();
+      }, 1000 * 30);
+    });
 
-  bot.on("error", (err) => {
-    console.log(err);
-    console.log("Reconnecting...");
-    setTimeout(() => {
-      init();
-    }, 1000 * 10);
-  });
+    // bot.on("error", (err) => {
+    //   console.log(err);
+    //   console.log("Reconnecting...");
+    //   setTimeout(() => {
+    //     init();
+    //   }, 1000 * 10);
+    // });
+  } catch (error) {
+    init();
+  }
 };
 
 init();
